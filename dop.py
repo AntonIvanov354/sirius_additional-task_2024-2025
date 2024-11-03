cb16965aa4fc7b9e2bb81cd3e01fc7a77c613079
AntonIvanov354 <adivanov2809@gmail.com>
2024-11-03 22:38:33 +0300

    Update dop.py

--- dop.py
+++ dop.py
@@ -6,7 +6,7 @@ disk_url = 'https://cloud-api.yandex.net/v1/disk'
 access_token = 'ff65e2c4918f40cd8a67447822b6618f'  # токен доступа к Диску
 video_files = []  # Список для хранения видео файлов
 
-def get_video_files(folder_path): #Получаем список видео файлов из папки на Яндекс.Диске
+def get_video_files(folder_path):  # Получаем список видео файлов из папки на Яндекс.Диске
     headers = {'Authorization': f'OAuth {access_token}'}
     params = {'path': folder_path}
     response = requests.get(f'{disk_url}/resources', headers=headers, params=params)
@@ -19,11 +19,11 @@ def get_video_files(folder_path): #Получаем список видео фа
     else:
         print("Ошибка при получении файлов с Яндекс.Диска:", response.content)
 
-def extract_audio_from_video(video_path, audio_path): # Извлекает аудио из видеофайла и сохраняет его.
+def extract_audio_from_video(video_path, audio_path):  # Извлекает аудио из видеофайла и сохраняет его.
     video = VideoFileClip(video_path)
     video.audio.write_audiofile(audio_path)
 
-def transcribe_audio(audio_path):# Преобразует аудио в текст с использованием Google Speech Recognition
+def transcribe_audio(audio_path):  # Преобразует аудио в текст с использованием Google Speech Recognition
     recognizer = sr.Recognizer()
     with sr.AudioFile(audio_path) as source:
         audio_data = recognizer.record(source)
@@ -36,7 +36,7 @@ def transcribe_audio(audio_path):# Преобразует аудио в текс
             print("Не удалось распознать аудио")
             return None
 
-def summarize_video(video_path):# функция для получения транскрипции видеофайла.
+def summarize_video(video_path):  # Функция для получения транскрипции видеофайла.
     audio_path = 'temp_audio.wav'
     extract_audio_from_video(video_path, audio_path)  # Извлекаем аудио
     transcription = transcribe_audio(audio_path)  # Транскрибируем аудио
@@ -49,4 +49,4 @@ if __name__ == '__main__':
     
     for video_url in video_files:
         transcription = summarize_video(video_url)  # Получаем транскрипцию для каждого видео
-        print(f"Транскрипция для {video_url}:\n{transcription}") 
+        print(f"Транскрипция для {video_url}:n{transcription}") 
